f6bfad0583a366e0fba6f3aff1525914afdef8f4
Istiyaq Ahmad <ahmedistiyaqqq03@gmail.com>
2020-03-28 16:13:58 +0000

    l

---
+++ scripts/hG/jfGfmdnnfgsDFhefs032q3vwvvrRE23fcwe.js
@@ -0,0 +1,14 @@
+function start(){
+  var m1 = document.getElementById("m1");
+
+  m1.addEventListener("mouseover", m1);
+  m1.addEventListener("mouseout", m1O);
+}
+
+function m1{
+    m1.src = "https://s2contentimage.netlify.com/image-poster/Ky/813hj3b23924n.png";
+}
+
+function m1O{
+    m1.src = "https://s2contentimage.netlify.com/image-poster/K7/823h4289ynz7838.png";
+}
