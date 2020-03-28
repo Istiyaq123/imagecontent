d6e4ce9a2c2d9d7cd671f98be4081061a9d3a050
Istiyaq Ahmad <ahmedistiyaqqq03@gmail.com>
2020-03-28 16:18:54 +0000

    l

--- scripts/hG/jfGfmdnnfgsDFhefs032q3vwvvrRE23fcwe.js
+++ scripts/hG/jfGfmdnnfgsDFhefs032q3vwvvrRE23fcwe.js
@@ -1,14 +1,14 @@
 function start(){
   var m1 = document.getElementById("m1");
 
-  m1.addEventListener("mouseover", m1);
-  m1.addEventListener("mouseout", m1O);
+  m1.addEventListener("mouseover", movie1);
+  m1.addEventListener("mouseout", movie1O);
 }
 
-function m1{
+function movie1{
     m1.src = "https://s2contentimage.netlify.com/image-poster/Ky/813hj3b23924n.png";
 }
 
-function m1O{
+function movie1O{
     m1.src = "https://s2contentimage.netlify.com/image-poster/K7/823h4289ynz7838.png";
 }
