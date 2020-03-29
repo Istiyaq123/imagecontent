0b1d44f290be5cd2b4ad356c6ee7977bdc6a2f10
Istiyaq Ahmad <ahmedistiyaqqq03@gmail.com>
2020-03-29 13:31:53 +0100

    l

--- scripts/hG/5MwI4C7BewuPVzjqbfI3rC0KHhrrKfdcclfoc8EF.js
+++ scripts/hG/5MwI4C7BewuPVzjqbfI3rC0KHhrrKfdcclfoc8EF.js
@@ -1,11 +1,14 @@
 function start(){
   var one = document.getElementById("1");
   var two = document.getElementById("2");
+  var three = document.getElementById("3");
 
   one.addEventListener("mouseover", oneI);
   one.addEventListener("mouseout", oneO);
   two.addEventListener("mouseover", twoI);
   two.addEventListener("mouseout", twoO);
+  three.addEventListener("mouseover", threeI);
+  three.addEventListener("mouseout", threeO);
   
   function oneO(){
     one.src = "https://s2contentimage.netlify.com/image-poster/ic/i28c7yo2dDj5L0e2vmQ4VRBkGY0sayO3BlEeQr3aAsnQPGA8tyeUt6oaIogR.png";
@@ -20,5 +23,12 @@ function start(){
 
   function twoI(){
     two.src = "https://s2contentimage.netlify.com/image-poster/K7/eJ2oGS8iI4OuuChxUDPsnoYRqyuWSOOlKEOzbvc5vMeTKakRpgK65remyhiE.png";
+  }
+  function threeO(){
+    three.src = "https://s2contentimage.netlify.com/image-poster/jg/N2BXYy0DpLjQNHNsbLZkQCby3axszCSU2qPVjta5DiVddvP5Jmx3Lcl4bZWI.png";
+  }
+
+  function threeI(){
+    three.src = "https://s2contentimage.netlify.com/image-poster/K7/MoS8b1qtCUBbYs8N7yZGwhDzq7bkMXly6MqLtb8ZhLowLSDGE49YFp0ve9Fw.png";
   }
 }
